6a872c5a57e406337c75026e0fb5381737600f6e
Artyom Nazarov <artyom.nazaroff@yandex.ru>
2022-03-22 16:44:27 +0300

    Updates

---
+++ precache-manifest.f11b1e933e8fe6401edf4b303f710bd6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "689957f0638bdf986f308f3243c4c1dc",
+    "url": "/hot-burgers/index.html"
+  },
+  {
+    "revision": "dbc280a6e11782e97497",
+    "url": "/hot-burgers/static/css/main.3d2a5f5c.chunk.css"
+  },
+  {
+    "revision": "3520a1b7586c937498c5",
+    "url": "/hot-burgers/static/js/2.60ddec70.chunk.js"
+  },
+  {
+    "revision": "dbc280a6e11782e97497",
+    "url": "/hot-burgers/static/js/main.e0980edb.chunk.js"
+  },
+  {
+    "revision": "a5a0e5b337eff91b963e",
+    "url": "/hot-burgers/static/js/runtime-main.eb76173a.js"
+  },
+  {
+    "revision": "385ae8f1201379005ad5f888db39e059",
+    "url": "/hot-burgers/static/media/burger-logo-4.385ae8f1.jpg"
+  },
+  {
+    "revision": "6a542800f80cfe80cc2eeafb41759854",
+    "url": "/hot-burgers/static/media/burger-title.6a542800.jpg"
+  },
+  {
+    "revision": "a37bfead7aee9510b324850af150a054",
+    "url": "/hot-burgers/static/media/burgernaya.a37bfead.jpg"
+  }
+]);
