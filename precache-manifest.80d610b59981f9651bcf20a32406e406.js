b72af7d6f80a7bb7c00f82bb9395262dc4f2bbf4
Artyom Nazarov <artyom.nazaroff@yandex.ru>
2022-03-22 23:40:46 +0300

    Updates

--- precache-manifest.80d610b59981f9651bcf20a32406e406.js
+++ precache-manifest.80d610b59981f9651bcf20a32406e406.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "2e36d9c7cc2ff465fa86a50234e2d6f4",
+    "revision": "9213a32a92680fa30d021b8e2a2cbb56",
     "url": "/hot-burgers/index.html"
   },
   {
-    "revision": "048c88634a163cd63956",
+    "revision": "c50010a2ee2853c3a13e",
     "url": "/hot-burgers/static/css/main.3d2a5f5c.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/hot-burgers/static/js/2.60ddec70.chunk.js"
   },
   {
-    "revision": "048c88634a163cd63956",
-    "url": "/hot-burgers/static/js/main.7fbe0d7b.chunk.js"
+    "revision": "c50010a2ee2853c3a13e",
+    "url": "/hot-burgers/static/js/main.ff81c270.chunk.js"
   },
   {
     "revision": "a5a0e5b337eff91b963e",
